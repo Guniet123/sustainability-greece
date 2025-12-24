cc1178886ef38eb2a7ea2b64a183ef51c38e65c8
Guniet Dhillon <gunietdhillon@gmail.com>
2025-12-23 20:00:22 -0800

    Locations interface update

---
+++ server/src/data/locations.ts
@@ -0,0 +1,18 @@
+import { Location } from "../types/location";
+
+export const locations: Location[] = [
+    {
+        id: "1",
+        category: "solar",
+        latitude: 40.6401,
+        longitude: 22.9444,
+        description: "Government-installed solar power plants."
+    },
+    {
+        id: "2",
+        category: "windmill",
+        latitude: 35.3387,
+        longitude: 25.1442,
+        description: "Government-installed windmills."
+    }
+];
---
+++ server/src/types/location.ts
@@ -0,0 +1,7 @@
+export interface Location {
+    category: "windmill" | "recycling" | "leiki" | "solar";
+    latitude: number;
+    longitude: number;
+    description: string;
+    id: string;
+}
