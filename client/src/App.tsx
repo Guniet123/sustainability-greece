fcb365b7e4060aef5ce2a4d66a78a5164b8d0102
Guniet Dhillon <gunietdhillon@gmail.com>
2026-01-02 01:34:09 -0800

    Links bug fix 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,4 @@
-/* import Home from "./pages/Home";
-import Gilman from "./pages/Gilman";
-import Trips from "./pages/Trips";
-import { useJsApiLoader } from "@react-google-maps/api";
-
-const libraries: ("marker")[] = ["marker"];
-
-export default function App() {
-const path = window.location.pathname;
-
-if (path === "/gilman") {
-  return <Gilman />;
-}
-
-if (path === "/trips") {
-  return <Trips />
-}
-
-  const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
-  if (loadError) {
-    return (
-      <div style={{ color: "red", padding: "20px"}}>
-        Failed to load Google Maps
-      </div>
-    );
-  }
-
-  if (!isLoaded) {
-    return <div style={{color: "white", padding: "20px"}}>Loading map...</div>
-  }
-
-  return <Home />;
-} */
-
-  import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Gilman from "./pages/Gilman";
 import Trips from "./pages/Trips";
@@ -63,11 +26,7 @@ export default function App() {
 
   return (
     <Router>
-      <nav style={{ display: "flex", gap: "20px", padding: "10px" }}>
-        <Link to="/">Home</Link>
-        <Link to="/gilman">Gilman</Link>
-        <Link to="/trips">Our Trips</Link>
-      </nav>
+      
 
       <Routes>
         <Route path="/" element={<Home />} />
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,3 @@
-/* export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="navbar-inner">
-                <a href="https://guniet123.github.io" className="navbar-name" target="_blank" rel="noopener noreferrer">
-                    Guniet Dhillon
-                </a>
-            
-                <div className="navbar-links">
-                    <a href="/">Home</a>
-                    <a href="/gilman">Gilman</a>
-                    <a href="/trips">Our Trips</a>
-                </div>
-            </div>
-        </nav>
-    );
-} */
-
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
