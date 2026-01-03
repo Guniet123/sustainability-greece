2c29be32df51e4d93e087f485b2b8edd4e2bc9de
Guniet Dhillon <gunietdhillon@gmail.com>
2026-01-02 17:45:02 -0800

    Map notice update

---
+++ client/src/components/Mapbar.tsx
@@ -0,0 +1,7 @@
+export default function Mapbar() {
+    return (
+        <div className="mapbar">
+        <h2 className="mapbar-content">Please allow ~30 seconds for map markers to load. Thank you!</h2>
+        </div>
+    );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import ScrollSection from "../components/ScrollSection";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Mapbar from "../components/Mapbar";
 
 export default function Home() {
     return (
         <>
             <Navbar />
+
+            <Mapbar />
 
             <Hero />
 
