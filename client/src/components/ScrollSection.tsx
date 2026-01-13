ea7c28e0fba56b5b65af37049f052b6a88b74a5c
Guniet Dhillon <gunietdhillon@gmail.com>
2026-01-12 21:32:39 -0800

    Client commit

--- client/src/components/ScrollSection.tsx
+++ client/src/components/ScrollSection.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import LocationsMap from "../components/LocationsMap";
 import type { Location } from "../types/location";
+import { locations } from "../locations";
 
 const sections = [
     {
@@ -85,12 +86,14 @@ export default function ScrollSection() {
 
     const sectionRefs = useRef<(HTMLElement | null)[]>([]);
 
-    useEffect(() => {
+    /* useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/api/locations`)
       .then((res) => res.json())
       .then((data: Location[]) => setAllLocations(data))
       .catch((err) => console.error("Failed to fetch locations:", err));
-  }, []);
+  }, []); */
+
+    setAllLocations(locations);
     
     useEffect(() => {
         const handleScroll = () => {
